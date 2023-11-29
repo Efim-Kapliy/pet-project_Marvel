4de26031614c1070ae6b14ef1a7e4c2a9571f851
Efim-Kapliy <efim0424@mail.ru>
2023-11-29 13:47:11 +0300

    refactor: change file structure and add images.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./components/app/App";
+// import "./style/style.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
