e7353f23768e4def542c1b453888cbf7b3917d66
Efim-Kapliy <efim0424@mail.ru>
2023-11-30 13:18:24 +0300

    refactor: change the RandomChar function to RandomChar class

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,43 +1,51 @@
+import { Component } from "react";
 import "./randomChar.scss";
-import thor from "../../resources/img/thor.jpeg";
+
 import mjolnir from "../../resources/img/mjolnir.png";
 
-const RandomChar = () => {
-  return (
-    <div className="randomchar">
-      <div className="randomchar__block">
-        <img src={thor} alt="Random character" className="randomchar__img" />
-        <div className="randomchar__info">
-          <p className="randomchar__name">Thor</p>
-          <p className="randomchar__descr">
-            As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted
-            hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's quite smart and
-            compassionate...
-          </p>
-          <div className="randomchar__btns">
-            <a href="#" className="button button__main">
-              <div className="inner">homepage</div>
-            </a>
-            <a href="#" className="button button__secondary">
-              <div className="inner">Wiki</div>
-            </a>
+class RandomChar extends Component {
+  state = {
+    name: null,
+    description: null,
+    thumbnail: null,
+    homepage: null,
+    wiki: null,
+  };
+
+  render() {
+    const { name, description, thumbnail, homepage, wiki } = this.state;
+    return (
+      <div className="randomchar">
+        <div className="randomchar__block">
+          <img src={thumbnail} alt="Random character" className="randomchar__img" />
+          <div className="randomchar__info">
+            <p className="randomchar__name">{name}</p>
+            <p className="randomchar__descr">{description}</p>
+            <div className="randomchar__btns">
+              <a href={homepage} className="button button__main">
+                <div className="inner">homepage</div>
+              </a>
+              <a href={wiki} className="button button__secondary">
+                <div className="inner">Wiki</div>
+              </a>
+            </div>
           </div>
         </div>
+        <div className="randomchar__static">
+          <p className="randomchar__title">
+            Random character for today!
+            <br />
+            Do you want to get to know him better?
+          </p>
+          <p className="randomchar__title">Or choose another one</p>
+          <button className="button button__main">
+            <div className="inner">try it</div>
+          </button>
+          <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
+        </div>
       </div>
-      <div className="randomchar__static">
-        <p className="randomchar__title">
-          Random character for today!
-          <br />
-          Do you want to get to know him better?
-        </p>
-        <p className="randomchar__title">Or choose another one</p>
-        <button className="button button__main">
-          <div className="inner">try it</div>
-        </button>
-        <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default RandomChar;
