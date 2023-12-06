299235ab32560813644c86a657e4b0c3a530a9fa
Efim-Kapliy <efim0424@mail.ru>
2023-12-06 21:06:10 +0300

    feat: add limit of comics to 12 and adapt photos to CharInfo

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -72,10 +72,16 @@ class CharInfo extends Component {
 
 const View = ({ char }) => {
   const { name, description, thumbnail, homepage, wiki, comics } = char;
+
+  const noImg = {};
+  thumbnail === "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+    ? (noImg.objectFit = "fill")
+    : (noImg.objectFit = "cover");
+
   return (
     <>
       <div className="char__basics">
-        <img src={thumbnail} alt={name} />
+        <img src={thumbnail} alt={name} style={noImg} />
         <div>
           <div className="char__info-name">{name}</div>
           <div className="char__btns">
@@ -91,7 +97,8 @@ const View = ({ char }) => {
       <div className="char__descr">{description}</div>
       <div className="char__comics">Comics:</div>
       <ul className="char__comics-list">
-        {comics.map((item, i) => {
+        {comics.length > 0 ? null : "No comics related to this character have been found."}
+        {comics.slice(0, 12).map((item, i) => {
           return (
             <li key={i} className="char__comics-item">
               {item.name}
