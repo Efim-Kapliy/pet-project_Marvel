8fcd32ab6c3f0a12c5d5f43e43e3536e9a3ff0d2
Efim-Kapliy <efim0424@mail.ru>
2024-02-14 13:32:37 +0300

    feat: add and configure Formik and Yup in CharSearchForm

--- src/components/charSearchForm/CharSearchForm.js
+++ src/components/charSearchForm/CharSearchForm.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { Formik, Form, Field, ErrorMessage as FormikErrorMessage } from "formik";
+import * as Yup from "yup";
 
 import "./charSearchForm.scss";
 
@@ -7,18 +9,39 @@ const CharSearchForm = () => {
 
   return (
     <div className="char__search">
-      <h3 className="char__search-name">Or find a character by name:</h3>
-      <div className="char__search-flex">
-        <input type="text" className="char__field" />
-        <button className="button button__main">
-          <div className="inner">FIND</div>
-        </button>
+      <label className="char__search-name" htmlFor="charName">
+        Or find a character by name:
+      </label>
+      <Formik
+        initialValues={{ name: "" }}
+        validationSchema={Yup.object({
+          charName: Yup.string()
+            .matches(/^\D*$/, "String characters only")
+            .min(2, "Too Short!")
+            .max(50, "Too Long!")
+            .required("This field is required"),
+        })}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form className="char__search-flex">
+            <Field id="charName" className="char__field" type="text" name="charName" placeholder="Enter name" />
+            <button className="button button__main" type="submit" disabled={isSubmitting}>
+              <div className="inner">FIND</div>
+            </button>
+            <FormikErrorMessage className="error error__text" name="charName" component="div" />
+          </Form>
+        )}
+      </Formik>
+      <div className="char__search-valid">
         <div className="valid valid__text">There is! Visit NAME page?</div>
         <a href={toPage} className="button button__secondary">
           <div className="inner">TO PAGE</div>
         </a>
-        <div className="error error__text">The character was not found. Check the name and try again</div>
       </div>
+      <div className="error error__text">The character was not found. Check the name and try again</div>
     </div>
   );
 };
